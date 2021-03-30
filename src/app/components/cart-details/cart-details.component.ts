16e92674e6a5c67008f157d6e42849dbbe1417a7
hengalvin <alvin.hadiheng@gmail.com>
2021-03-30 22:04:22 +0700

    cart item details and increment quantity

---
+++ src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit {
+
+  constructor(private cartService: CartService) { }
+
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+
+    // get a handle to return the cart items 
+    this.cartItems = this.cartService.cartItems;
+
+    // subscribe to the cart total price
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    // subscribe to the cart total quantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    )
+
+    // compute the total cart item and quantity
+    this.cartService.computeCartTotal();
+  }
+
+  incrementQuantity(theCartItem: CartItem) {
+    this.cartService.addToCart(theCartItem);
+  }
+}
+ 
